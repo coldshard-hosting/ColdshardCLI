760eae473bfda1318e319cbd7ed91fc779d29ba4
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2023-02-04 15:53:10 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coldshard-cli',
-    version='0.1.2',
+    version='0.1.3',
     packages=["cli", "cli.utils"],
     install_requires=[
         'Click',
