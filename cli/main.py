b0f6445c966b959a3f4536cc878f8b38525846e0
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2023-02-04 14:07:54 +0000

    Add ability to see account information

--- cli/main.py
+++ cli/main.py
@@ -13,18 +13,21 @@ def core():
     click.echo(click.style("Welcome to the ColdShard CLI!", fg="green", bold=True)) 
 
 @core.command()
-@click.argument("api_key")
+@click.option("--api-key", prompt="API Key", hide_input=True)
 def login(api_key: str):
     """Login to the panel using your API Key"""
 
-    response = request("GET", "account")
+    response = request("GET", "account", api_key=api_key)
 
     if not response:
         return # The request was not made because there was no API key.
 
+    data = response.json()
+    account = data["attributes"]
+
     with open("../config.json", "w") as f:
         json.dump({"api_key": api_key}, f, indent=4)
-    click.echo(click.style("Successfully logged in", fg="green", bold=True))
+    click.echo(click.style(f"Successfully logged in as {account['email']}", fg="green", bold=True))
 
 @core.command()
 def logout():
@@ -33,3 +36,20 @@ def logout():
         json.dump({}, f, indent=4)
     click.echo(click.style("Successfully logged out", fg="green", bold=True))
 
+@core.command("account")
+def account():
+    """Get information about your account."""
+    response = request("GET", "account")
+
+    if not response:
+        return
+
+    data = response.json()
+    account = data["attributes"]
+
+    info: str = ""
+
+    info += click.style(f"Email: {account['email']}\n", fg="blue", bold=True)
+    info += click.style(f"Username: {account['username']}\n", fg="blue", bold=True)
+
+    click.echo(info)
--- cli/utils/__init__.py
+++ cli/utils/__init__.py
@@ -7,32 +7,36 @@ PANEL_API_URL: str = "https://panel.coldshard.com/api/client"
 
 def is_logged_in() -> bool:
     """Check if the user is logged in."""
-    with open("../config.json", "r") as f:
-        config = json.load(f)
-    if config.get("api_key"):
-        return True
-    return False
+    try:
+        with open("../config.json", "rb", ) as f:
+            config = json.load(f)
 
-def get_api_key() -> Optional[str]:
+        if not config.get("api_key"):
+            return False
+        return True
+    except FileNotFoundError:
+        return False
+
+def get_api_key() -> str:
     """Get the API Key from the config file."""
-    logged_in = is_logged_in()
-    if not logged_in:
-        return None
     with open("../config.json", "r") as f:
         config = json.load(f)
+
     return config["api_key"]
 
 def warn_not_logged_in() -> None:
     click.echo(click.style("You are not logged in.", fg="red", bold=True))
 
-
-def request(method: str, endpoint: str, **kwargs) -> Optional[requests.Response]:
+def request(method: str, endpoint: str, *, api_key: Optional[str] = None, **kwargs) -> Optional[requests.Response]:
     """Make a request to the panel."""
-    api_key: Optional[str] = get_api_key()
 
     if not api_key:
-        warn_not_logged_in()
-        return
+
+        if not is_logged_in():
+            warn_not_logged_in()
+            return
+
+        api_key = get_api_key()
 
     response = requests.request(
         method, f"{PANEL_API_URL}/{endpoint}", headers={"Authorization": f"Bearer {api_key}"}, **kwargs
